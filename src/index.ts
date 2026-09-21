3275f35fdeb8133b63ee7efcb610f1b272e99beb
Mohamed Staali <imthetigerz@gmail.com>
2026-09-21 23:14:06 +0000

    feat: build server skeleton + main function to run the server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-//this file will start the server and read the stdin
+import { McpServer } from "@modelcontextprotocol/server";
+import { StdioServerTransport } from "@modelcontextprotocol/server/stdio";
+
+//create server instance
+const server = new McpServer({
+    version: "1.0.0",
+    name: "github",
+})
+
+//main function to run the server
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("GitHub MCP Server is running on stdio");
+}
+
+main().catch((error) => {
+    console.error("Fatal: error at main(): ", error);
+})
